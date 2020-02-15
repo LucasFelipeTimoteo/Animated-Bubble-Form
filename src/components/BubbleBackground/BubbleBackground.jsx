41057137c698816acdeec394813299f01f6fe217
Lucas Felipe <conhecimento54321@gmail.com>
2020-02-14 23:34:45 -0300

    Create BubbleBackground component

---
+++ src/components/BubbleBackground/BubbleBackground.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const BubbleBackground = () => {
+    return ( 
+        <div className="bubbleBackground">
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+        </div>
+     );
+}
+ 
+export default BubbleBackground;
