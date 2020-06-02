d604e56667a10f61d176126ee31b3bfcfc066c50
Lucas Felipe <conhecimento54321@gmail.com>
2020-06-02 12:54:28 -0300

    add controlled components pattern

--- src/components/TransparentForm/FormBody/FormBody.jsx
+++ src/components/TransparentForm/FormBody/FormBody.jsx
@@ -4,10 +4,6 @@ import Inputs from '../Inputs/Inputs'
 import Button from '../Button/Button'
 
 class FormBody extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
     render() { 
         return ( 
             <form className="form">
--- src/components/TransparentForm/Inputs/Inputs.jsx
+++ src/components/TransparentForm/Inputs/Inputs.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Inputs.css'
 
-const Inputs = () => {
-    return (
-        <React.Fragment>
-            <div class="name">
-                <label htmlFor="name">
-                    <i class="fa fa-user"></i>
-                </label>
-                <input placeholder="name..." type="text" id="name" />
-            </div>
+class Inputs extends Component {
 
-            <div class="password">
-                <label htmlFor="password">
-                    <i class="fa fa-lock"></i>
-                </label>
-                <input placeholder="password..." type="password" id="password" />
-            </div>
-        </React.Fragment>
-    );
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "",
+            password: "",
+        };
+        
+        this.setName = this.setName.bind(this);
+        this.setPassword = this.setPassword.bind(this);
+    }
+
+    setName(event) {
+        this.setState({ name: event.target.value })
+    };
+
+    setPassword(event) {
+        this.setState({ password: event.target.value })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div class="name">
+                    <label htmlFor="name">
+                        <i class="fa fa-user"></i>
+                    </label>
+                    <input placeholder="name..." type="text" id="name" value={this.state.name} onChange={this.setName} />
+                </div>
+
+                <div class="password">
+                    <label htmlFor="password">
+                        <i class="fa fa-lock"></i>
+                    </label>
+                    <input placeholder="password..." type="password" id="password" value={this.state.password} onChange={this.setPassword} />
+                </div>
+            </React.Fragment>
+        );
+    };
 }
 
 export default Inputs;
